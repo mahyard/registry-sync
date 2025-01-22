3892cd8e73adb34b2520d864aacbdba7b1a11e00
Mahyar Damavand <mahyar.d.zig@gmail.com>
2025-01-22 17:04:59 +0330

    Produce metrics for monitoring with prometheus

--- export-artifactory.sh
+++ export-artifactory.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Script to export Docker images from JFrog Artifactory.
+# Script to export Docker images from JFrog Artifactory and generate Prometheus metrics.
 # Author: Mahyar Damavand
 # Date: 2024-12-29
-# Version: 1.0
+# Version: 1.1
 
 # Import credentials if exists
 [ -f .env ] && source .env
@@ -12,6 +12,7 @@
 DEFAULT_JFROG_URL="jfrog.example.com"
 DEFAULT_TARGET_DIR="/tmp/registry-sync"
 DEFAULT_JF_ACCESS_TOKEN="JF-ACCESS-TOKEN"
+DEFAULT_METRICS_FILE="/var/log/registry-sync-metrics.prom"
 
 # Constants
 JFROG_URL=${JFROG_URL:-$DEFAULT_JFROG_URL}
@@ -20,6 +21,13 @@ HEADER_TYPE="Content-type: text/plain"
 JF_ACCESS_TOKEN=${JF_ACCESS_TOKEN:-$DEFAULT_JF_ACCESS_TOKEN}
 HEADER_AUTH="Authorization: Bearer $JF_ACCESS_TOKEN"
 TARGET_DIR=${TARGET_DIR:-$DEFAULT_TARGET_DIR}
+METRICS_FILE=${METRICS_FILE:-$DEFAULT_METRICS_FILE}
+
+# Metrics variables
+script_start_time=$(date +%s)
+success_count=0
+failure_count=0
+overall_status=0
 
 # Function for logging
 log_info() {
@@ -29,6 +37,35 @@ log_info() {
 log_error() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: $1" >&2
 }
+
+# Function to write metrics to file
+write_metrics() {
+    local script_end_time=$(date +%s)
+    local script_duration=$((script_end_time - script_start_time))
+
+    cat <<EOF > "$METRICS_FILE"
+# HELP jfrog_sync_last_timestamp_seconds Unix timestamp of the last script execution
+# TYPE jfrog_sync_last_timestamp_seconds gauge
+
+# HELP jfrog_sync_last_duration_seconds Duration of the last script execution in seconds
+# TYPE jfrog_sync_last_duration_seconds gauge
+
+# HELP jfrog_sync_images_total Total number of images processed by the script
+# TYPE jfrog_sync_images_total counter
+
+# HELP jfrog_sync_status Sync script execution status (1 = successful, 0 = unsuccessful).
+# TYPE jfrog_sync_status gauge
+
+jfrog_sync_last_timestamp_seconds{job="export",repo="docker"} $script_start_time
+jfrog_sync_last_duration_seconds{job="export",repo="docker"} $script_duration
+jfrog_sync_images_total{job="export",repo="docker",status="success"} $success_count
+jfrog_sync_images_total{job="export",repo="docker",status="failure"} $failure_count
+jfrog_sync_status{job="export",repo="docker"} $overall_status
+EOF
+}
+
+# Trap to ensure metrics are written on exit
+trap 'overall_status=$(($? == 0 ? 1 : 0)); write_metrics' EXIT
 
 # Check user running the script
 if [ "$(id --user --name)" != "root" ]; then
@@ -71,7 +108,7 @@ if [ -z "$images" ]; then
 fi
 
 # Make sure TARGET_DIR exists
-mkdir -p $TARGET_DIR
+mkdir -p "$TARGET_DIR"
 
 # Export images
 for img in $images; do
@@ -82,8 +119,10 @@ for img in $images; do
     regctl image export -p local --compress "$image_ref" > "$image_file"
     if [ $? -ne 0 ]; then
         log_error "Failed to export image: $image_ref"
+        ((failure_count++))
         continue
     fi
+    ((success_count++))
 done
 
 log_info "Docker image export process completed successfully."
--- import-artifactory.sh
+++ import-artifactory.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-# Script to import Docker images into JFrog Artifactory.
+# Script to import Docker images into JFrog Artifactory and generate Prometheus metrics.
 # Author: Mahyar Damavand
 # Date: 2024-12-29
-# Version: 1.0
+# Version: 1.1
 
-# Imporet credentials if exists
+# Import credentials if exists
 [ -f .env ] && source .env
 
 # Defaults
 DEFAULT_JFROG_URL="jfrog.example.com"
 DEFAULT_TARGET_DIR="/tmp/registry-sync"
+DEFAULT_METRICS_FILE="/var/log/registry-sync-metrics.prom"
 
 # Constants
 JFROG_URL=${JFROG_URL:-$DEFAULT_JFROG_URL}
 TARGET_DIR=${TARGET_DIR:-$DEFAULT_TARGET_DIR}
+METRICS_FILE=${METRICS_FILE:-$DEFAULT_METRICS_FILE}
+
+# Metrics variables
+script_start_time=$(date +%s)
+success_count=0
+failure_count=0
+overall_status=0
 
 # Function for logging
 log_info() {
@@ -25,6 +33,28 @@ log_error() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR: $1" >&2
 }
 
+# Function to write metrics to file
+write_metrics() {
+    local script_end_time=$(date +%s)
+    local script_duration=$((script_end_time - script_start_time))
+
+    # Merge metrics from this script and export script into one
+    # and store it in the node-exporter's textfile collector's directory
+    metrics_file_basename=$(basename $METRICS_FILE)
+    cat "$METRICS_FILE" - <<EOF > /var/lib/prometheus/node-exporter/$metrics_file_basename
+
+jfrog_sync_last_timestamp_seconds{job="import",repo="docker"} $script_start_time
+jfrog_sync_last_duration_seconds{job="import",repo="docker"} $script_duration
+jfrog_sync_images_total{job="import",repo="docker",status="success"} $success_count
+jfrog_sync_images_total{job="import",repo="docker",status="failure"} $failure_count
+jfrog_sync_status{job="import",repo="docker"} $overall_status
+EOF
+}
+
+# Trap to ensure metrics are written on exit
+trap 'overall_status=$(($? == 0)); write_metrics' EXIT
+trap 'overall_status=$(($? == 0)); write_metrics' EXIT
+
 # Check user running the script
 if [ "$(id --user --name)" != "root" ]; then
     log_error "Script can only be run as root"
@@ -32,9 +62,6 @@ if [ "$(id --user --name)" != "root" ]; then
 fi
 
 log_info "Starting Docker image import process."
-
-# Make sure TARGET_DIR exists
-mkdir -p $TARGET_DIR
 
 # Import images
 for img in $(ls "$TARGET_DIR"/docker-image-*tar.gz 2>/dev/null); do
@@ -45,8 +72,10 @@ for img in $(ls "$TARGET_DIR"/docker-image-*tar.gz 2>/dev/null); do
     regctl image import "$image_ref" "$img"
     if [ $? -ne 0 ]; then
         log_error "Failed to import image: $img"
+        ((failure_count++))
         continue
     fi
+    ((success_count++))
 done
 
 log_info "Docker image import process completed successfully."
